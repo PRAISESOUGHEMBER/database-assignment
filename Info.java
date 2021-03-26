d27f6a691b68904ff17556d6436e824791d1b51c
PRAISESOUGHEMBER <78384754+PRAISESOUGHEMBER@users.noreply.github.com>
2021-03-26 12:36:37 +0100

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redis.workings;
+import java.util.HashMap;
+/**
+ *
+ * @author Tom Sough
+ */
+class Info {
+     static HashMap<Double, String> map = new HashMap<>();
+    public Info(){
+    
+    
+    map.put(22,446,554,864.37, "Lagos state");
+    map.put(2,052,569,760.34, "Jigawa state");
+    map.put(3,793,162,871.00, "Imo state");
+    map.put(2,877,514,521.00, "Anambra state");
+    map.put(22,446,554,864.37, "Fct");
+    map.put(5,516,236,256.00, "Zamfara state");
+    map.put(3,888,108,415.27, "Oyo state");
+    map.put(3,888,108,415.27, "Niger state");
+    map.put(7,396,560,094.34, "Rivers state");
+    map.put(4,480,169,796.26, "Nasarawa state");
+    map.put(5,516,236,256.00, "Ekiti state" );
+    map.put(5,516,236,256.00, "Katsina state");
+    map.put(22,446,554,864.37, "Abia state");
+    map.put(22,446,554,864.37, "Borno state");
+    map.put(2,713,519,569.98, "Osun state");
+    map.put(7,396,560,094.34, "Ogun state");
+    map.put(4,480,169,796.26, "Edo state");
+    map.put(4,480,169,796.26, "Ondo state");
+    map.put(4,480,169,796.26, "Enugu state");
+    map.put(1,047,216,746.00, "Kwara state");
+    map.put(3,888,108,415.27, "Delta state");
+    map.put(2,713,519,569.98, "Kano state");
+    map.put(1,047,216,746.00, "Yobe state");
+    map.put(2,713,519,569.98, "Akwa Ibom state");
+    map.put(28,149,481,840.76, "Bauchi state");
+    map.put(150,596,076,871.98, "Plateau state");
+    map.put(8,229,330,228.34, "Kaduna state");
+    map.put(1,047,216,746.00, "Kogi state");
+    map.put(22,446,554,864.37, "Adamawa state");
+    map.put(7,831,757,412.37, "Ebonyi state");
+    map.put(3,705,166,312.27, "Cross River state");
+    map.put(7,396,560,094.34, "Bayelsa state");
+    map.put(2,696,860,998.00, "Benue state");
+    map.put(150,596,076,871.98, "Gombe state");
+    map.put(8,229,330,228.34, "Kebbi state");
+    map.put(150,596,076,871.98, "Sokoto state");
+    map.put(1,047,216,746.00, "Taraba state");
+}
+}
